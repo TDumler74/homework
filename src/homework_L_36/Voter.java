d2bf77dedbe942df94ec0d03d5958090d0f0f7e0
TDumler74 <tatjana.dumler@gmail.com>
2023-03-08 22:03:10 +0100

    sec

---
+++ src/homework_L_36/Voter.java
@@ -0,0 +1,52 @@
+package homework_L_36;
+
+public class Voter {
+
+    private String name;
+    private String address;
+    int age;
+
+    public Voter(String name, String address, int age) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        if (address != address) {
+            System.out.println("Welcome!");
+            this.address = address;
+        } else {
+            System.out.println("Sorry, you are in wrong voter district!");
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age <= 16) {
+            this.age = age;
+            System.out.println("The voter is to young!");
+        } else {
+            System.out.println("Welcome!");
+        }
+    }
+
+    void reaction() {
+        System.out.println("You have to leave.");
+        System.out.println(name + ": Ok, buy.");
+    }
+}
